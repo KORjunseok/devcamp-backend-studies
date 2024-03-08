7fafb17bfe7a3d74f9a15acbb294ae0f57ea5cf7
오준석 <y22zzp@naver.com>
2024-03-09 00:50:25 +0900

    Day5 검증 로직 추가 및 쿠키 생성
    쿠키 정보 저장 (Respond), auth.http 테스트 추가

--- 과제/nest-dev-camp/src/auth/auth.controller.ts
+++ 과제/nest-dev-camp/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, Response } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/user.dto';
 
@@ -7,7 +7,27 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register') // register 주소의 POST 요청 처리
-  async register(@Body() userDto : CreateUserDto) {
+  async register(@Body() userDto: CreateUserDto) {
     return await this.authService.register(userDto);
+  }
+
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    // validateUser 호출 유저 정보 획인
+    const userInfo = await this.authService.validateUser(
+      req.body.email,
+      req.body.password,
+    );
+
+    // 유저 정보 있을 시 쿠키 정보를 Response에 저장
+    if (userInfo) {
+      res.cookie('login', JSON.stringify(userInfo), {
+        httpOnly: false, // 브라우저에서 읽을 수 있게 함
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      });
+      return res.send({ message: 'login success' });
+    } else {
+      return res.status(400).send({ message: 'login fail' });
+    }
   }
 }
--- 과제/nest-dev-camp/src/auth/auth.service.ts
+++ 과제/nest-dev-camp/src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import * as argon2 from 'argon2';
 import { CreateUserDto } from 'src/user/user.dto';
 
 
+
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
@@ -35,4 +36,24 @@ export class AuthService {
       throw new HttpException('서버 에러', 500);
     }
   }
+
+    // 검증 로직 validater 
+  async validateUser (email: string, password: string) {
+    const user = await this.userService.getUser(email);
+    // 이메일로 유저 정보 확인
+    if (!user) { // user 없을 시 검증 실패
+      return null;
+    }
+    const { password: hashedPassword, ...userInfo } = user;
+    // 패스워드 확인
+    if (await argon2.verify(hashedPassword, password)) {
+      // password match
+      return userInfo;
+    } else {
+      // password did not match
+      return null;
+    }
+
+  }
 }
+
